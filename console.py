b3cb1a357db8059c3f83f1ca5471952f0a63f980
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-04-20 00:01:44 +0300

    testing show method

--- console.py
+++ console.py
@@ -5,7 +5,7 @@
 
 import cmd
 from models.base_model import BaseModel
-from sys import argv
+from datetime import datetime
 from models import storage
 
 
@@ -14,6 +14,9 @@ class HBNBCommand(cmd.Cmd):
         A class representing the console
     """
     prompt = '(hbnb) '
+    classes = {
+        'BaseModel': BaseModel
+    }
 
     def do_EOF(self, arg):
         """ EOF command interpreter """
@@ -25,7 +28,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, arg):
         """
-        Create a new instance of the BaseModel class
+        Creates a new instance of the BaseModel class
         and saves it to a json file and prints the id
         """
         if not arg:
@@ -58,7 +61,8 @@ class HBNBCommand(cmd.Cmd):
             if key not in storage.all():
                 print("** no instance found **")
             else:
-                print(storage.all()[key])
+                each_obj = storage.all()[key]
+                print(each_obj)
 
     def do_destroy(self, arg):
         """
@@ -80,30 +84,88 @@ class HBNBCommand(cmd.Cmd):
                 print("** no instance found **")
             else:
                 del storage.all()[key]
+                storage.save()
 
     def do_all(self, arg):
         """
-            prints all instances based on class name or
+            prints all instance objs based on class name or
             'all' command
         """
-        obj_array = []
-        all_objs = storage.all()
+        obj_list = []
+        if arg == "":
+            all_objs = storage.all()
+            for obj in all_objs.values():
+                obj_dict = obj.to_dict()
+                obj_dict.pop('__class__', None)
+                for key, value in obj_dict.items():
+                    if key in ['created_at', 'updated_at']:
+                        obj_dict[key] = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                obj_str = f"[{obj.__class__.__name__}] ({obj.id}) {obj_dict}"
+                obj_list.append(obj_str)
+            print(obj_list)
+        else:
+            args = arg.split()
+            class_name = args[0]
+            if class_name in self.classes:
+                all_objs = storage.all()
+                for obj in all_objs.values():
+                    obj_dict = obj.to_dict()
+                    obj_dict.pop('__class__', None)
+                    for key, value in obj_dict.items():
+                        if key in ['created_at', 'updated_at']:
+                            obj_dict[key] = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    obj_str = f"[{obj.__class__.__name__}] ({obj.id}) {obj_dict}"
+                    obj_list.append(obj_str)
+                print(obj_list)
+            else:
+                print(" ** class doesn't exist ** ")
 
-        if arg == '':
-            for key in all_objs.keys():
-                obj = all_objs[key]
-                obj_array.append(str(obj))
-            print(obj_array)
+    def do_update(self, arg):
+        """
+            updates the instance based on the class name and id
+            by adding or updating attribute and
+            save the changes to json file
+        """
+        args = arg.split()
+        print(args)
+        if len(args) == 0:
+            print("** class name missing **")
+        class_name = args[0]
+        if class_name not in self.classes:
+            print("** class doesn't exist **")
+        if len(args) == 1:
+            print("** instance id missing **")
+        obj_id = args[1]
+        input_key = class_name + '.' + obj_id
+        all_objs = storage.all()
+        print(input_key in all_objs)
+
+        if input_key not in all_objs:
+            print("** no instance found **")
+        if len(args) == 2:
+            print("** attribute name missing **")
+        if len(args) == 3:
+            print(" ** value missing ** ")
         else:
             try:
-                class_obj = eval(arg)
-                for key in all_objs.keys():
-                    obj = all_objs[key]
-                    if type(obj) == class_obj:
-                        obj_array.append(str(obj))
-                print(obj_array)
-            except NameError:
-                print("** class doesn't exist **")
+                attr_name = args[2]
+                attr_value_str = args[3]
+                if attr_name in ['id', 'created_at', 'updated_at']:
+                    print("** cannot update attribute **")
+                    return
+                for o in all_objs.values():
+                    obj_dict = o.to_dict()
+                    obj_dict.pop('__class__', None)
+                    for key, value in obj_dict.items():
+                        if key in ['created_at', 'updated_at']:
+                            obj_dict[key] = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+
+
+                obj = all_objs[input_key]
+                setattr(obj, attr_name, attr_value_str)
+                obj.save()
+            except ValueError:
+                pass
 
 
 if __name__ == '__main__':
